ae65939f07adf2bd72dbf2a2c35801415262ea15
sartre <sartre@kraken>
2018-05-03 03:54:46 -0300

    Prueba

--- src/vista/inicio.java
+++ src/vista/inicio.java
@@ -53,6 +53,7 @@ public class inicio {
         f.getSj().getJuegos().add(j2);
         new InicioPantalla().setVisible(true);
         
+        System.out.println("______________");
         System.out.println("~ Prueba Git ~");
     }
     
